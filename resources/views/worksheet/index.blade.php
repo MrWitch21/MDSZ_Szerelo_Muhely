71a47f1017046eda1ff0b21c1425d1d1e8aa4723
M1st3r <93176257+MrWitch21@users.noreply.github.com>
2024-04-30 11:20:08 +0200

    search-bar alpha

--- resources/views/worksheet/index.blade.php
+++ resources/views/worksheet/index.blade.php
@@ -3,32 +3,35 @@
         <div class="relative w-full mt-14">
             <div>
                 <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-                    <div class="flex justify-between items-center pb-4 rounded-t  dark:border-gray-600 p-4">
+                    <div class="flex justify-between items-center pb-4 rounded-t border-b dark:border-gray-600 p-4">
                         <h3 class="text-3xl font-semibold text-gray-900 dark:text-white">
                             Folyamatban lévő munkalapok
                         </h3>
-                    </div>
-                    <div
-                        class="flex flex-col md:flex-row items-center justify-end md:space-x-3 space-y-3 md:space-y-0 mx-4 py-4 border-t dark:border-gray-700">
-                        <div class="flex-grow"></div>
-                        <div>
-                            <button id="filterDropdownButton" data-dropdown-toggle="filterDropdown"
-                                class="py-2 px-4 flex items-center text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-                                    class="h-4 w-4 mr-1.5 -ml-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                Szűrési beállítások
-                                <svg class="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                    <path clip-rule="evenodd" fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
-                                </svg>
-                            </button>
+                        <div class="w-full md:w-1/4">
+                            <form class="flex items-center">
+                                <label for="search" class="sr-only">Keresés</label>
+                                <div class="relative w-full">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                                            viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <input type="text" id="search"
+                                        class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                        placeholder="Keresés" required="">
+                                </div>
+                                <div class="flex justify-end" style="align-items: flex-end;">
+                                    <button type="submit"
+                                        class="ml-4 bg-blue-500 text-white font-semibold h-10 px-4 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-300 ease-in-out">
+                                        Keresés</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
+
                     <div class="overflow-x-auto">
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead
@@ -108,7 +111,7 @@
                                                         class="bg-lime-600 text-white font-semibold h-10 px-4 rounded-lg shadow-md hover:bg-lime-400 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 transition duration-300 ease-in-out flex items-center">
                                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 mr-1 -ml-0.5">
                                                             <path fill-rule="evenodd" d="M8 1a3.5 3.5 0 0 0-3.5 3.5V7A1.5 1.5 0 0 0 3 8.5v5A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 11.5 7V4.5A3.5 3.5 0 0 0 8 1Zm2 6V4.5a2 2 0 1 0-4 0V7h4Z" clip-rule="evenodd" />
-                                                          </svg>                                                                                                                    
+                                                          </svg>
                                                         Lezárás
                                                     </button>
                                                     </a>
--- routes/web.php
+++ routes/web.php
@@ -26,22 +26,6 @@ Route::get('/', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('/');
 
-Route::get('/create', function () {
-    return view('create');
-})->middleware(['auth', 'verified'])->name('create');
-
-Route::get('/active', function () {
-    return view('active');
-})->middleware(['auth', 'verified'])->name('active');
-
-Route::get('/closed', function () {
-    return view('closed');
-})->middleware(['auth', 'verified'])->name('closed');
-
-Route::get('/service', function () {
-    return view('service');
-})->middleware(['auth', 'verified'])->name('service');
-
 Route::group(['middleware' => ['auth', 'mechanic']], function () {
 });
 Route::group(['middleware' => ['auth', 'receptionist']], function () {
@@ -49,34 +33,18 @@ Route::group(['middleware' => ['auth', 'receptionist']], function () {
 
 Route::resource('component', ComponentController::class)
     ->only('index', 'create', 'store');
-// Route::get('/component', [ComponentController::class, 'index']);
-// Route::get('/component/create',[ComponentController::class, 'create']);
-// Route::post('/component', [ComponentController::class, 'store']);
-// Route::get('/component/{id}/edit', [ComponentController::class, 'edit']);
-// Route::put('/component/{id}', [ComponentController::class, 'update']);
-// Route::delete('/component/{id}', [ComponentController::class, 'destroy']);
 
 Route::resource('material', MaterialController::class)
     ->only('index', 'create', 'store');
-// Route::get('/material', [MaterialController::class, 'index']);
-// Route::get('/material/create',[MaterialController::class, 'create']);
-// Route::post('/material', [MaterialController::class, 'store']);
-// Route::get('/material/{id}/edit', [MaterialController::class, 'edit']);
-// Route::put('/material/{id}', [MaterialController::class, 'update']);
-// Route::delete('/material/{id}', [MaterialController::class, 'destroy']);
 
 Route::resource('work_process', WorkProcessController::class)
     ->only('index', 'create', 'store');
-// Route::get('/work_process', [WorkProcessController::class, 'index']);
-// Route::get('/work_process/create',[WorkProcessController::class, 'create']);
-// Route::post('/work_process', [WorkProcessController::class, 'store']);
-// Route::get('/work_process/{id}/edit', [WorkProcessController::class, 'edit']);
-// Route::put('/work_process/{id}', [WorkProcessController::class, 'update']);
-// Route::delete('/work_process/{id}', [WorkProcessController::class, 'destroy']);
 
+//closed worksheet operations
 Route::get('/worksheet/closed', [WorksheetController::class, 'closed'])->name('worksheet.closed');
 Route::get('/worksheet/closed/{worksheetid}', [WorksheetController::class, 'closedShow'])->name('worksheet.closed.show');
 
+//Base operations
 Route::get('/worksheet', [WorksheetController::class, 'index']);
 Route::get('/worksheet/create', [WorksheetController::class, 'create']);
 Route::post('/worksheet', [WorksheetController::class, 'store']);
@@ -84,21 +52,25 @@ Route::get('/worksheet/{worksheetid}', [WorksheetController::class, 'show'])->na
 Route::get('/worksheet/{worksheetid}/edit', [WorksheetController::class, 'edit'])->name('worksheet.edit');
 Route::put('/worksheet/{worksheetid}', [WorksheetController::class, 'update']);
 
+//worksheet closing
 Route::get('/worksheet/{worksheetid}/edit/closing', [WorksheetController::class, 'closing'])->name('worksheet.closing');
 Route::put('/worksheet/{worksheetid}/edit/close', [WorksheetController::class, 'close']);
 
+//CRUD components to the worksheet
 Route::get('/worksheet/{worksheetid}/component/add', [WorksheetController::class, 'componentCreate'])->name('worksheet.components_add');
 Route::post('/worksheet/{worksheetid}/component/store', [WorksheetController::class, 'componentStore']);
 Route::get('/worksheet/{worksheetid}/edit/{component_worksheetid}/component', [WorksheetController::class, 'componentsEdit'])->name('worksheet.components_edit');
 Route::put('/worksheet/{worksheetid}/{component_worksheetid}/component/update', [WorksheetController::class, 'componentsUpdate']);
 Route::delete('/worksheet/{worksheetid}/{component_worksheetid}/component/destroy', [WorksheetController::class, 'componentDestroy']);
 
+//CRUD materials to the worksheet
 Route::get('/worksheet/{worksheetid}/material/add', [WorksheetController::class, 'materialCreate'])->name('worksheet.materials_add');
 Route::post('/worksheet/{worksheetid}/material/store', [WorksheetController::class, 'materialStore']);
 Route::get('/worksheet/{worksheetid}/edit/{material_worksheetid}/material', [WorksheetController::class, 'materialsEdit'])->name('worksheet.materials_edit');
 Route::put('/worksheet/{worksheetid}/{material_worksheetid}/material/update', [WorksheetController::class, 'materialsUpdate']);
 Route::delete('/worksheet/{worksheetid}/{material_worksheetid}/material/destroy', [WorksheetController::class, 'materialDestroy']);
 
+//CRUD work_processes to the worksheet
 Route::get('/worksheet/{worksheetid}/work_process/add', [WorksheetController::class, 'work_processCreate'])->name('worksheet.work_processes_add');
 Route::post('/worksheet/{worksheetid}/work_process/store', [WorksheetController::class, 'work_processStore']);
 Route::get('/worksheet/{worksheetid}/edit/{work_process_worksheetid}/work_process', [WorksheetController::class, 'work_processesEdit'])->name('worksheet.work_processes_edit');
